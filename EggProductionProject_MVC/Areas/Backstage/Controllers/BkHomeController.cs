c54d61516c251b42bcc3f96a08d679bce3441ff3
CHUYITING <luke52070000@gmail.com>
2024-08-06 21:18:00 +0800

    加入專案檔案。

---
+++ EggProductionProject_MVC/Areas/Backstage/Controllers/BkHomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EggProductionProject_MVC.Areas.Backstage.Controllers
+{
+    public class BkHomeController : Controller
+    {
+        [Area("Backstage")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EggProductionProject_MVC/Areas/Frontstage/Controllers/FtHomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EggProductionProject_MVC.Areas.Frontstage.Controllers
+{
+	public class FtHomeController : Controller
+	{
+		[Area("Frontstage")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
