e9fa46545c2410de08456496e6dd620d45c6e0f3
nora <hweng028@gmail.com>
2024-08-09 15:58:01 +0800

    v修改暫存

--- EggProductionProject_MVC/Areas/Backstage/Controllers/CouponTypesController.cs
+++ EggProductionProject_MVC/Areas/Backstage/Controllers/CouponTypesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using EggProductionProject_MVC.Models;
 using EggProductionProject_MVC.Areas.Backstage.ViewModels;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 
 namespace EggProductionProject_MVC.Areas.Backstage.Controllers
 {
@@ -54,62 +55,48 @@ namespace EggProductionProject_MVC.Areas.Backstage.Controllers
         }
 
 
-        //[HttpPost]
-        //public IActionResult CreateOrEdit(CouponType model)
-        //{
-        //    if (model.CouponTypeNo == 0) // Assuming Id = 0 means it's a new item
-        //    {
-        //        // 执行创建逻辑
-        //        _context.CouponTypes.Add(model);
-        //        _context.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    else
-        //    {
-        //        // 执行编辑逻辑
-        //        var existingModel = _context.CouponTypes.Find(model.CouponTypeNo);
-        //        if (existingModel == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        existingModel.Name = model.Name;
-        //        existingModel.Minimum = model.Minimum;
-        //        _context.Update(existingModel);
-        //        _context.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //}
-
-        // GET: Backstage/CouponTypes/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: CouponTypes/Edit/5
+        public async Task<IActionResult> Edit(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var couponType = await _context.CouponTypes
-                .Include(c => c.PublicStatusNoNavigation)
-                .FirstOrDefaultAsync(m => m.CouponTypeNo == id);
+            var couponType = await _context.CouponTypes.FindAsync(id);
             if (couponType == null)
             {
                 return NotFound();
             }
-
-            return View(couponType);
+            return PartialView("_EditForm", couponType);
         }
 
-        // GET: Backstage/CouponTypes/Create
-        public IActionResult Create()
+        // POST: CouponTypes/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<JsonResult> Edit(CouponType couponType)
         {
-            ViewData["PublicStatusNo"] = new SelectList(_context.PublicStatuses, "PublicStatusNo", "PublicStatusNo");
-            return View();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (couponType.CouponTypeNo == 0) // 新建
+                    {
+                        _context.Add(couponType);
+                    }
+                    else // 更新
+                    {
+                        _context.Update(couponType);
+                    }
+                    await _context.SaveChangesAsync();
+                    return Json(new { success = true });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false, message = ex.Message });
+                }
+            }
+
+            // 如果模型状态无效，返回错误信息
+            var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToArray();
+            return Json(new { success = false, message = string.Join(", ", errors) });
         }
 
-        // POST: Backstage/CouponTypes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("CouponTypeNo,Name,Price,Minimum,PublicStatusNo,StartTime,EndTime,UseAlone,EmployeeSid")] CouponType couponType)
@@ -124,305 +111,8 @@ namespace EggProductionProject_MVC.Areas.Backstage.Controllers
             return View(couponType);
         }
 
-        // GET: Backstage/CouponTypes/Edit/5
-
-        public async Task<IActionResult> GetEditForm(int id)
-        {
-            var couponType = await _context.CouponTypes.FindAsync(id);
-            if (couponType == null)
-            {
-                return NotFound();
-            }
-
-            return PartialView("_EditForm", couponType);
-        }
-
-
-
-        //[HttpPost]
-        //public async Task<IActionResult> Edit(CouponType model)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        // 如果模型无效，返回部分视图以显示错误c
-        //        return PartialView("_EditForm", model);
-        //    }
-
-        //    var couponType = await _context.CouponTypes.FindAsync(model.CouponTypeNo);
-        //    if (couponType == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    // 更新 couponType 的属性
-        //    couponType.Name = model.Name;
-        //    couponType.PublicStatusNo = model.PublicStatusNo;
-
-        //    // 保存更改
-        //    await _context.SaveChangesAsync();
-
-        //    // 返回成功的响应（可以是 JSON，也可以是简单的消息）
-        //    return Json(new { success = true });
-        //}
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    //if (id == null)
-        //    //{
-        //    //    return NotFound();
-        //    //}
-
-        //    //var couponType = await _context.CouponTypes.FindAsync(id);
-        //    //if (couponType == null)
-        //    //{
-        //    //    return NotFound();
-        //    //}
-        //    //ViewData["PublicStatusNo"] = new SelectList(_context.PublicStatuses, "PublicStatusNo", "PublicStatusNo", couponType.PublicStatusNo);
-        //    return View();
-        //}
-
-        // POST: Backstage/CouponTypes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("CouponTypeNo,Name,Price,Minimum,PublicStatusNo,StartTime,EndTime,UseAlone,EmployeeSid")] CouponType couponType)
-        //{
-        //    if (id != couponType.CouponTypeNo)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(couponType);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            //if (!CouponTypeExists(couponType.CouponTypeNo))
-        //            //{
-        //            //    return NotFound();
-        //            //}
-        //            //else
-        //            //{
-        //            //    throw;
-        //            //}
-        //        }
-        //        //return RedirectToAction(nameof(Index));
-        //        return PartialView("_EditForm", couponType);
-        //    }
-        //    ViewData["PublicStatusNo"] = new SelectList(_context.PublicStatuses, "PublicStatusNo", "PublicStatusNo", couponType.PublicStatusNo);
-        //    return View(couponType);
-        //}
-
-        [HttpGet]
-        public async Task<IActionResult> Edit(int id)
-        {
-            var couponType = await _context.CouponTypes.FindAsync(id);
-            if (couponType == null)
-            {
-                return NotFound();
-            }
-
-
-            return PartialView("_EditForm", couponType); // 返回部分视图
-            //return View(couponType);
-        }
-
-        // POST: /CouponTypes/Edit
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("CouponTypeNo,Name,Price,Minimum,PublicStatusNo,StartTime,EndTime,UseAlone,EmployeeSid")] CouponType couponType)
-        //{
-        //    if (id != couponType.CouponTypeNo)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //    {
-        //        _context.Update(couponType);
-        //        await _context.SaveChangesAsync();
-        //        return Json(new { success = true });
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        return Json(new { success = false, message = "Update failed." });
-        //    }
-        //    }
-
-        //    ViewData["PublicStatusNo"] = new SelectList(_context.PublicStatuses, "PublicStatusNo", "PublicStatusNo", couponType.PublicStatusNo);
-        //    return Json(new { success = false, message = "Update failed." });
-        //}
-
-
-        //成功
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit([Bind("CouponTypeNo,Name,Price,Minimum,PublicStatusNo,StartTime,EndTime,UseAlone,EmployeeSid")] CouponType couponType)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(couponType);
-        //            await _context.SaveChangesAsync();
-        //            return PartialView("_EditForm", couponType);  // 返回 JSON 响应以指示成功
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            return PartialView("_EditForm", couponType);  // 返回 JSON 响应以指示失败
-        //        }
-        //    }
-        //    return PartialView("_EditForm", couponType); // 返回 JSON 响应以指示模型状态无效
-        //}
-
-
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit([Bind("CouponTypeNo,Name,Price,Minimum,PublicStatusNo,StartTime,EndTime,UseAlone,EmployeeSid")] CouponType couponType)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(couponType);
-                    await _context.SaveChangesAsync();
-                    //return Json(new { success = true }); // 返回 JSON 响应以表示成功
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    return Json(new { success = false, message = "Update failed." });
-                }
-            }
-            //return Json(new { success = false, message = "Model state is invalid." });
-            return View(couponType);
-        }
-
-
-
-
-
-
-
-
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("CouponTypeNo,Name,Price,Minimum,PublicStatusNo,StartTime,EndTime,UseAlone,EmployeeSid")] CouponType couponType)
-        //{
-        //    if (id != couponType.CouponTypeNo)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(couponType);
-        //            await _context.SaveChangesAsync();
-        //            return Json(new { success = true }); // 返回 JSON 响应以指示成功
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            return Json(new { success = false, message = "Update failed." }); // 返回 JSON 响应以指示失败
-        //        }
-        //    }
-
-        //    ViewData["PublicStatusNo"] = new SelectList(_context.PublicStatuses, "PublicStatusNo", "PublicStatusNo", couponType.PublicStatusNo);
-        //    return PartialView("_EditForm", couponType); // 返回部分视图
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var couponType = await _context.CouponTypes.FindAsync(id);
-        //    if (couponType == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    ViewData["PublicStatusNo"] = new SelectList(_context.PublicStatuses, "PublicStatusNo", "PublicStatusNo", couponType.PublicStatusNo);
-        //    return PartialView("_EditForm", couponType); // 返回部分视图
-        //}
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("CouponTypeNo,Name,Price,Minimum,PublicStatusNo,StartTime,EndTime,UseAlone,EmployeeSid")] CouponType couponType)
-        //{
-        //    if (id != couponType.CouponTypeNo)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(couponType);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!CouponTypeExists(couponType.CouponTypeNo))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["PublicStatusNo"] = new SelectList(_context.PublicStatuses, "PublicStatusNo", "PublicStatusNo", couponType.PublicStatusNo);
-        //    return PartialView("_EditForm", couponType);
-        //}
-
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var couponType = await _context.CouponTypes
-                .Include(c => c.PublicStatusNoNavigation)
-                .FirstOrDefaultAsync(m => m.CouponTypeNo == id);
-            if (couponType == null)
-            {
-                return NotFound();
-            }
-
-            return View(couponType);
-
-        }
-
-        // POST: Backstage/CouponTypes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var couponType = await _context.CouponTypes.FindAsync(id);
-            if (couponType != null)
-            {
-                _context.CouponTypes.Remove(couponType);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+       
+      
 
         private bool CouponTypeExists(int id)
         {
