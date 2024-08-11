40d9c218e7969151fa92137aad84c8c16efb10e8
Johnson <johnson041804182@gmail.com>
2024-08-11 15:25:20 +0800

    金銀蛋CRUD與EDIT的圖片上傳完成(大略)

---
+++ EggProductionProject_MVC/wwwroot/js/memberPicUpload.js
@@ -0,0 +1,25 @@
+﻿function previewImage(inputFile, img, btn) {
+    var file = inputFile.files[0];
+
+    if (inputFile.files[0] != null) {
+        var allowType = "image.*";
+        if (file.type.match(allowType)) {
+            //預覽
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                img.attr("src", e.target.result);
+                img.attr("title", file.name);
+            };
+            reader.readAsDataURL(file);
+
+
+            btn.prop("disabled", false);
+        }
+        else {
+            alert("不允許的檔案類型");
+            $("#btnUpdate").prop("disabled", true);
+            $("#Picture").val("");
+        }
+    }
+
+}
