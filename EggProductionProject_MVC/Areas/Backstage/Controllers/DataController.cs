0aa28223fa2b8d74f0593447eb936d7b05208415
heartfuldays <heartfuldays307@gmail.com>
2024-08-19 10:28:53 +0900

    先註解未完成功能

--- EggProductionProject_MVC/Areas/Backstage/Controllers/DataController.cs
+++ EggProductionProject_MVC/Areas/Backstage/Controllers/DataController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EggProductionProject_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.Security.Policy;
 
 namespace EggProductionProject_MVC.Areas.Backstage.Controllers
 {
@@ -6,9 +10,40 @@ namespace EggProductionProject_MVC.Areas.Backstage.Controllers
     [Area("Backstage")]
     public class DataController : Controller
     {
+        private EggPlatformContext _context;
+
+        public DataController(EggPlatformContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        /*public IActionResult Export()
+        {
+            ExcelPackage pck = new ExcelPackage();
+            var worksheet = pck.Workbook.Worksheets.Add("Sheet1");
+
+
+            var f = from c in _context.DailyChickAmountsRates
+                    select new
+                    {
+
+                    };
+
+
+            var stream = new MemoryStream();
+            pck.SaveAs(stream);
+            stream.Position = 0;
+
+            // 返回 Excel 文件
+            var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            var fileName = "exported_data.xlsx";
+
+            return File(stream, contentType, fileName);
+        }*/
     }
 }
