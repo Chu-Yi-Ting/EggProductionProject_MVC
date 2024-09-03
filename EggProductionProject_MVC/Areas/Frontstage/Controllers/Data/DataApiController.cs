8df7f6c862b1dce9e1d98df8cd509d2866c24222
heartfuldays <heartfuldays307@gmail.com>
2024-09-03 15:11:01 +0800

    chart 更新一版

--- EggProductionProject_MVC/Areas/Frontstage/Controllers/Data/DataApiController.cs
+++ EggProductionProject_MVC/Areas/Frontstage/Controllers/Data/DataApiController.cs
@@ -8,6 +8,8 @@ using System.Data;
 using System.Drawing;
 using Microsoft.Extensions.Configuration;
 using System.Globalization;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using EggProductionProject_MVC.ViewModels;
 
 namespace EggProductionProject_MVC.Areas.Frontstage.Controllers.Data
 {
@@ -21,93 +23,8 @@ namespace EggProductionProject_MVC.Areas.Frontstage.Controllers.Data
         {
             _context = context;
         }
-
-        /*public IActionResult Get_House()
-        {
-            var House = (from m in _context.ChickHouses
-                         where m.MemberSid == 1 //需抓瀏覽器session的id
-                         select new
-                         {
-                             value = m.HouseSid,
-                             text = m.HouseName
-                         }).Distinct();
-
-            return Json(House);
-        }
-
-        [HttpGet]
-        public IActionResult Export_Chart()
-        {
-            try
-            {
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                ExcelPackage pck = new ExcelPackage();
-                DataTable dt = new DataTable();
-                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("ChickenFarmData");
-                string ConnectionString = @"Data Source=.;Initial Catalog=EggPlatform;Integrated Security=True;TrustServerCertificate=True;";
-
-                string query = @"SELECT Name '會員名字',
-                                 HouseName '雞舍名字',
-                                 DeathRate as '死亡率(%)', 
-                                 EggRate as '產蛋率(%)',
-                                 d.UnQRate as '蛋不合格率(%)',
-                                 RemainingCount as '剩餘雞隻',
-                                 DeathAmount as '死亡數',
-                                 EggAmount as '當日產蛋數',
-                                 UnQAmount as '當日不合格蛋數',
-                                 SubcategoryName as '生產總類',
-                                 Weekage as 週齡,
-                                 convert(varchar,Date,111) '日期'
-                                 FROM DailyChickAmountsRate d
-							     where MemberSid = 1  
-                                       and HouseSid = 1
-                                 ORDER BY MemberSid,HouseSid,Date ";
-
-                List<SqlParameter> paras = new List<SqlParameter>();
-
-                using (SqlConnection con = new SqlConnection(ConnectionString))
-                {
-                    using (SqlCommand cmd = new SqlCommand(query, con))
-                    {
-                        if (paras != null)
-                            cmd.Parameters.AddRange(paras.ToArray());
-                        SqlDataAdapter da = new SqlDataAdapter(cmd);
-                        da.Fill(dt);
-                    }
-                }
-
-                // Load the data from the datatable
-                ws.Cells["A1"].LoadFromDataTable(dt, true);
-
-                // Format the header for column
-                using (ExcelRange rng = ws.Cells["A1:Z1"])
-                {
-                    rng.Style.Font.Bold = true;
-                    rng.Style.Fill.PatternType = ExcelFillStyle.Solid;
-                    rng.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(79, 129, 189));
-                    rng.Style.Font.Color.SetColor(Color.White);
-                }
-
-                ws.Cells[ws.Dimension.Address].AutoFitColumns();
-
-                var stream = new MemoryStream();
-                pck.SaveAs(stream);
-                stream.Position = 0;
-
-                // 返回 Excel 文件
-                var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-                var fileName = "exported_data.xlsx";
-                return File(stream, contentType, fileName);
-
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Operation failed.");
-            }
-
-        }*/
-
-
+        
+//-----------------------------------行事曆------------------------------------------------
         [HttpPost]
         public async Task<IActionResult> InsertCalendar([FromBody] Models.Calendar Calendar)
         {
@@ -214,6 +131,68 @@ namespace EggProductionProject_MVC.Areas.Frontstage.Controllers.Data
                 return StatusCode(500, ex);
             }
         }
+
+//---------------------------------圖表--------------------------------------------------
+
+        [HttpGet]
+        public async Task<IActionResult> Get_House()
+        {
+            var House = await ((from m in _context.ChickHouses
+                                where m.MemberSid == 1 //需抓瀏覽器session的id
+                                select new
+                                {
+                                    value = m.HouseSid,
+                                    text = m.HouseName
+                                }).Distinct().ToListAsync());
+
+            return Json(House);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Update_Chart(int memberSid,int Judge,int HouseSid,string StartDate, string EndDate)
+        {
+            var result = new EggProductionReport();
+            try { 
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("GetDailyData", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@Judge", Judge);
+                        cmd.Parameters.AddWithValue("@MemberSid", memberSid);
+                        cmd.Parameters.AddWithValue("@HouseSid", HouseSid);
+                        cmd.Parameters.AddWithValue("@StartDate", !string.IsNullOrWhiteSpace(StartDate) ? StartDate : DBNull.Value);
+                        cmd.Parameters.AddWithValue("@EndDate", !string.IsNullOrWhiteSpace(EndDate) ? EndDate : DBNull.Value);
+                        conn.Open();
+
+                        using (var reader = await cmd.ExecuteReaderAsync())
+                        {
+                            while (reader.Read())
+                            {
+                                // 設置標題（生產總類）為第一行的值
+                                result.Title = reader["生產總類"].ToString();
+
+                                // 添加每行數據到數據列表
+                                result.Rate.Add(new
+                                {
+                                    date = reader["日期"].ToString(),
+                                    value = Convert.ToDouble(reader["Result"]) // 確保轉換為數值格式
+                                });
+                            }
+                        }
+                    }
+                }
+
+                return Json(result);
+            }catch (Exception ex) {
+                return StatusCode(500, ex);
+            }
+
+        }
+
+
 
     }
 }
---
+++ EggProductionProject_MVC/ViewModels/EggProductionReport.cs
@@ -0,0 +1,16 @@
+﻿using EggProductionProject_MVC.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EggProductionProject_MVC.ViewModels
+{
+    public class EggProductionReport
+    {
+        public string Title { get; set; }
+        public List<object> Rate { get; set; }
+
+        public EggProductionReport()
+        {
+            Rate = new List<object>();
+        }
+    }
+}
