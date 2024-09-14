433aa741418b3acb35dce714d1ca3da9d52c65a9
CHUYITING <luke52070000@gmail.com>
2024-09-14 13:16:20 +0800

    完成商品上架分頁做一個重整的按鈕，使用ajax去更新當前上架的資料，排序測試正確

--- EggProductionProject_MVC/Areas/Frontstage/Controllers/StoreController.cs
+++ EggProductionProject_MVC/Areas/Frontstage/Controllers/StoreController.cs
@@ -20,53 +20,6 @@ namespace EggProductionProject_MVC.Areas.Frontstage.Controllers
             _userManager = userManager;
         }
 
-		//查詢賣家資訊及賣家的商品
-		//public IActionResult Store(int storeSid)
-		//{
-		//    var storeInfo = _context.Stores
-		//        .Where(s => s.StoreSid == storeSid)
-		//        .Select(s => new StoreViewModel
-		//        {
-		//            storeSid = s.StoreSid,
-		//            memberSid = s.MemberSid,
-		//            storeImagePath = s.StoreImagePath,
-		//            storeName = s.Company,
-		//            establishDate = s.EstablishDate,
-		//            storeIntroduction = s.StoreIntroduction,
-		//            productCount = _context.Products.Where(p => p.StoreSid == s.StoreSid).Count()
-		//        })
-		//        .FirstOrDefault();
-
-		//    if (storeInfo == null)
-		//    {
-		//        return NotFound();
-		//    }
-
-		//    // 查詢該賣家的所有商品
-		//    var products = _context.Products
-		//        .Where(p => p.StoreSid == storeSid && p.PublicStatusNo == 1)
-		//        .Select(p => new ProductViewModel
-		//        {
-		//            productSid = p.ProductSid,
-		//            productImagePath = _context.ProductImages
-		//                .Where(img => img.ProductSid == p.ProductSid)
-		//                .Select(img => img.ProductImagePath)
-		//                .FirstOrDefault(),
-		//            productName = p.ProductName,
-		//            price = p.Price
-		//        })
-		//        .ToList();
-
-		//    // 組合商品及賣家資訊
-		//    var ProductInStore = new ProductAndStoreViewModel
-		//    {
-		//        storeInfo = storeInfo,
-		//        products = products
-		//    };
-
-		//    return View(ProductInStore);
-		//}
-
 		// 查詢賣家資訊及賣家的商品
 		public IActionResult Store(StoreQueryDTO _storeQueryDTO)
 		{
@@ -280,8 +233,20 @@ namespace EggProductionProject_MVC.Areas.Frontstage.Controllers
                     .ToList();
             }
 
-			// 查詢所有子分類並傳送到前端
-			ViewBag.Subcategories = _context.ProductSubcategories.Select(s => new
+
+            // 判斷是否是 AJAX 請求
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return Json(new
+                {
+                    success = true,
+                    currentProducts = model.CurrentProducts,
+                    reviewingProducts = model.ReviewingProducts
+                });
+            }
+
+            // 查詢所有子分類並傳送到前端
+            ViewBag.Subcategories = _context.ProductSubcategories.Select(s => new
 			{
 				s.SubcategoryNo,
 				s.SubcategoryName
