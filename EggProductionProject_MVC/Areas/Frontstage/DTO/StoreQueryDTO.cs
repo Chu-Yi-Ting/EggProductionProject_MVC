448a425d7f97476b14df87444d90e4d53b5f5ddf
CHUYITING <luke52070000@gmail.com>
2024-09-15 00:40:07 +0800

    合併前將專案靜態文件圖片整理好並且都設為永遠複製，資料庫也更新資料

--- EggProductionProject_MVC/Areas/Frontstage/DTO/StoreQueryDTO.cs
+++ EggProductionProject_MVC/Areas/Frontstage/DTO/StoreQueryDTO.cs
@@ -4,6 +4,6 @@
     {
         public int storeSid { get; set; }
         public int page { get; set; } = 1; // 預設為第1頁
-        public int pageSize { get; set; } = 2; // 預設每頁9個商品
+        public int pageSize { get; set; } = 6; // 預設每頁9個商品
     }
 }
