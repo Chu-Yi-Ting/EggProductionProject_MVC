b387457a9fac1389202b9d3134f66c21a589902c
StatTreeTree <ho775593359@gmail.com>
2024-09-14 16:50:08 +0800

    新增影片成功

--- EggProductionProject_MVC/ViewModels/CreatVideoDTO.cs
+++ EggProductionProject_MVC/ViewModels/CreatVideoDTO.cs
@@ -1,7 +1,29 @@
-﻿namespace EggProductionProject_MVC.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EggProductionProject_MVC.ViewModels
 {
     public class CreatVideoDTO
     {
+       public int  CreatorSid { get; set; }
 
+        [Display(Name = "影片標題")]
+        public string VideoTitle { get; set; }
+
+        [Display(Name = "影片資訊")]
+       public string? InformationColumn { get; set; }
+
+
+        [Display(Name = "影片分類")]
+        public int? NatureSid { get; set; }
+
+        [Display(Name = "影片封面圖")]
+        public string? VideoCoverImage { get; set; }
+
+        [Display(Name = "影片內容")]
+        public string? MoviePath { get; set; }
+
+
+        [Display(Name = "影片上傳時間")]
+        public DateTime UploadDate { get; set; }
     }
 }
---
+++ EggProductionProject_MVC/ViewModels/LoginUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EggProductionProject_MVC.ViewModels
+{
+    public class LoginUserDTO
+    {
+        public int? userMemberSid { get; set; }
+
+        public string? UserName { get; set; }
+
+    }
+}
